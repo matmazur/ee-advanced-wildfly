7b8a4c85a5ff623ac8b2077b9ee4e4125cb88103
mateusz mazur <matmazur90@gmail.com>
2018-09-14 14:34:49 +0200

    proper reading of UserDao inject as UserDaoImpl

--- src/main/java/alternatives/service/UserService.java
+++ src/main/java/alternatives/service/UserService.java
@@ -1,4 +1,20 @@
 package alternatives.service;
 
+import alternatives.dao.UserDao;
+import alternatives.model.User;
+
+import javax.inject.Inject;
+
 public class UserService {
+
+    @Inject
+    private
+    UserDao userDao;
+
+
+    public void printUser(long id){
+
+        User user = userDao.read(id);
+        System.out.println(user);
+    }
 }
