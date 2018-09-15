da8d307d3b0693f97c3234e96bc278aa65a0596d
mateusz mazur <matmazur90@gmail.com>
2018-09-15 15:38:57 +0200

    added annotation in UserDaoImpl class

--- src/main/java/beans/UserDaoImpl.java
+++ src/main/java/beans/UserDaoImpl.java
@@ -1,9 +1,14 @@
 package beans;
 
+import interceptors.TimeLogger;
 import model.User;
 
 public class UserDaoImpl implements UserDao{
+
+
+
     @Override
+    @TimeLogger
     public User read(long id) {
         return new User(id,"John","Doe");
     }
